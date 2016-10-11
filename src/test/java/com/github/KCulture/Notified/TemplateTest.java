979a3be8fef107632cb99de1c71a22ff0f903f2e
lando <lburch@cardinalsolutions.com>
2016-10-11 11:03:10 -0400

    adding the source directory

---
+++ src/test/java/com/github/KCulture/Notified/TemplateTest.java
@@ -0,0 +1,12 @@
+package com.github.KCulture.Notified;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TemplateTest {
+    @Test
+    public void emptyTest() {
+       
+    }
+}
