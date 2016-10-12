cf5f36f1a2909392fe3af6ebe4a2f1fd84a2f8e6
lando <lburch@cardinalsolutions.com>
2016-10-12 09:01:29 -0400

    Added a test for EmailMessageService

--- src/test/java/com/github/KCulture/Notified/TemplateTest.java
+++ src/test/java/com/github/KCulture/Notified/TemplateTest.java
@@ -1,12 +1,15 @@
 package com.github.KCulture.Notified;
 
 import org.junit.Test;
+import org.junit.Assert;
 
-import static org.junit.Assert.*;
+import com.github.KCulture.Notified.Services.EmailMessageService;
+
 
 public class TemplateTest {
     @Test
     public void emptyTest() {
-       
+    	EmailMessageService se = new EmailMessageService();
+    	Assert.assertTrue(se.loadProps().containsKey("mail.smtp.host"));
     }
 }
