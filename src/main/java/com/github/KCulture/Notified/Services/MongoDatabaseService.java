f4971bf48c4ee4267987fe0bd23c65bf1991bf8c
lando <lburch@cardinalsolutions.com>
2016-10-13 15:49:56 -0400

    implemented listOfAppraised and coupled with a test

--- src/main/java/com/github/KCulture/Notified/Services/MongoDatabaseService.java
+++ src/main/java/com/github/KCulture/Notified/Services/MongoDatabaseService.java
@@ -1,29 +1,65 @@
 package com.github.KCulture.Notified.Services;
 
 import java.net.UnknownHostException;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.Date;
+import java.util.Calendar;
+
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.DB;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+
+
 
 import com.github.KCulture.Notified.Repository.Employee;
 
 public class MongoDatabaseService implements DatabaseService {
-
+	MongoClient mongoClient = null;
 	@Override
 	public int connect() {
-		int signal = 0;
+		this.initDatabase();
+	  return (this.mongoClient != null)?1:-1;
+	}
+	
+	private MongoClient initDatabase(){
 		try{
-			MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
-			signal = mongoClient.hashCode();
+			this.mongoClient = new MongoClient( "localhost" , 27017 );
 			// TODO: Build an adapter to encapsulate client 
 		}catch(UnknownHostException noHost){
-			signal = -1;
+			noHost.addSuppressed(noHost);
+			System.out.println(noHost.getMessage());
 		}
-		return signal;
+		return mongoClient;
 	}
-
+	
 	public List<Employee> listOfAppraised(int quarter){
-		return null; // TODO Stub needs to be replaced with implementation 
+		List<Employee> employees = new ArrayList<>();
+		this.initDatabase();
+		Calendar rightNow = Calendar.getInstance();
+	  rightNow.set(Calendar.MONTH,quarter);
+		DB db = mongoClient.getDB("test");
+	  DBCursor cursor = db.getCollection("employee").find();
+	  try {
+	     while(cursor.hasNext()) {
+	    	 DBObject object = (DBObject) cursor.next();
+	         Date date = (Date)object.get("hireDate");
+	         if(date.before(rightNow.getTime())){
+	        	 employees.add(this.toEmployee(object));
+	         }
+	     }
+	  } finally {
+	     cursor.close();
+	  }
+		return employees; 
+	}
+	
+	private Employee toEmployee(DBObject mongoObj){
+		
+		return new Employee(((String)mongoObj.get("firstName")),((String)mongoObj.get("lastName")) ,((Date) mongoObj.get("hireDate")));
 	}
 
 }
--- src/test/java/com/github/KCulture/Notified/TemplateTest.java
+++ src/test/java/com/github/KCulture/Notified/TemplateTest.java
@@ -1,14 +1,18 @@
 package com.github.KCulture.Notified;
 
-import org.junit.Test;
-import org.junit.Assert;
+import java.util.List;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.github.KCulture.Notified.Repository.Employee;
 import com.github.KCulture.Notified.Services.EmailMessageService;
 import com.github.KCulture.Notified.Services.MongoDatabaseService;
 
+
 public class TemplateTest {
     @Test
-    public void emptyTest() {
+    public void configFileLoadTest() {
     	EmailMessageService se = new EmailMessageService();
     	Assert.assertTrue(se.loadProps().containsKey("mail.smtp.host"));
     }
@@ -18,5 +22,18 @@ public class TemplateTest {
     	MongoDatabaseService mongoDBClient = new MongoDatabaseService();
     	int status =  mongoDBClient.connect();
     	Assert.assertTrue(status > -1);
+    }
+    @Test
+    public void sendMessageTest() {
+    	//TODO this will require setup of email account
+    	// EmailMessageService email = new EmailMessageService();
+    	// Assert.assertTrue(se.loadProps().containsKey("mail.smtp.host"));
+    }
+    
+    @Test
+    public void returned2EmployeeTest() {
+    	MongoDatabaseService mongoDBClient = new MongoDatabaseService();
+    	List<Employee> employees =  mongoDBClient.listOfAppraised(6);
+    	Assert.assertTrue(employees.size() == 2);
     }
 }
