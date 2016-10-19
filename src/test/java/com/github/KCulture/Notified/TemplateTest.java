0a32dafeb3515ec6c594b7a7145fd1909c710299
KCulture <lburch@cardinalsolutions.com>
2016-10-19 09:32:59 -0400

    testing our feature with array with empty string

--- src/test/java/com/github/KCulture/Notified/TemplateTest.java
+++ src/test/java/com/github/KCulture/Notified/TemplateTest.java
@@ -1,36 +1,26 @@
 package com.github.KCulture.Notified;
 
-import java.util.Calendar;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import org.bson.BSONObject;
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Assert;
 
-import com.github.KCulture.Notified.Repository.Employee;
-import com.github.KCulture.Notified.Services.EmailMessageService;
-import com.github.KCulture.Notified.Services.EmployeeSelectionStrategy;
-import com.github.KCulture.Notified.Services.MongoDatabaseService;
-import com.github.KCulture.Notified.Services.QuarterlyStrategy;
+import com.github.KCulture.Notified.Service.MongoDatabaseService;
+import com.github.KCulture.Notified.application.Notified;
 import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
 
 
 public class TemplateTest {
-    @Test
-    public void configFileLoadTest() {
-    	EmailMessageService se = new EmailMessageService();
-    	Assert.assertTrue(se.loadProps().containsKey("mail.smtp.host"));
-    }
-    
-    @Test
-    public void MongoTest() {
-    	MongoDatabaseService mongoDBClient = new MongoDatabaseService();
-    	int status =  mongoDBClient.connect();
-    	Assert.assertTrue(status > -1);
-    }
+//    @Test
+//    public void configFileLoadTest() {
+//    	EmailMessageService se = new EmailMessageService();
+//    	Assert.assertTrue(se.loadProps().containsKey("mail.smtp.host"));
+//    }
+//    
+//    @Test
+//    public void MongoTest() {
+//    	MongoDatabaseService mongoDBClient = new MongoDatabaseService();
+//    	int status =  mongoDBClient.connect();
+//    	Assert.assertTrue(status > -1);
+//    }
    // @Test
    // public void sendMessageTest() {
     	//TODO this will require setup of email account
@@ -64,19 +54,56 @@ public class TemplateTest {
 //  	boolean quarter =  mongoDBClient.isAppraisalDate(Calendar.getInstance().getTime());
 //  	Assert.assertTrue( quarter );
 //  }
-  @Test
-  public void findRightQuarterTest() {
-  	MongoDatabaseService mongoDBClient = new MongoDatabaseService();
-  	List<Employee> employees =  mongoDBClient.listOfAppraised(new QuarterlyStrategy());
-  	System.out.println(employees.get(0).hireDate);
-  	Assert.assertTrue( employees.size() == 1 );
-  }
-  
-  public void checknewDatabaseTest() {
-  	MongoDatabaseService mongoDBClient = new MongoDatabaseService();
-  	List<DBObject> employees =  mongoDBClient.writeAppraisableToStorage(new QuarterlyStrategy());
-  	 long count = mongoDBClient.getMongoDB().getCollection("appraised").count();
-  	Assert.assertTrue( employees.size() == count );
-  	mongoDBClient.getMongoDB().getCollection("appraised").remove(new BasicDBObject() );
-  }
+//  @Test
+//  public void findRightQuarterTest() {
+//  	MongoDatabaseService mongoDBClient = new MongoDatabaseService();
+//  	List<Employee> employees =  mongoDBClient.listOfAppraised(new QuarterlyStrategy());
+//  	System.out.println(employees.get(0).hireDate);
+//  	Assert.assertTrue( employees.size() == 1 );
+//  }
+//  
+//  public void checknewDatabaseTest() {
+//  	MongoDatabaseService mongoDBClient = new MongoDatabaseService();
+//  	List<DBObject> employees =  mongoDBClient.writeAppraisableToStorage(new QuarterlyStrategy());
+//  	 long count = mongoDBClient.getMongoDB().getCollection("appraised").count();
+//  	Assert.assertTrue( employees.size() == count );
+//  	mongoDBClient.getMongoDB().getCollection("appraised").remove(new BasicDBObject() );
+//  }
+//    @Test 
+//  public void viewToAddresses() throws AddressException {
+//  	EmailMessageService se = new EmailMessageService();	
+//  	Employee e = new Employee("a", "", Calendar.getInstance().getTime(), "a@acme.com");
+//  	Employee b = new Employee("a", "", Calendar.getInstance().getTime(), "b@acme.com");
+//  	List<Employee> aa = new ArrayList<>();
+//  	aa.add(e);aa.add(b);
+//  	Assert.assertTrue(se.getEmployeeEmails(aa).length == 2); 
+//  }
+//    @Test
+//  public void checkItem1() throws AddressException {
+//  	EmailMessageService se = new EmailMessageService();	
+//  	Employee e = new Employee("a", "", Calendar.getInstance().getTime(), "a@acme.com");
+//  	Employee b = new Employee("a", "", Calendar.getInstance().getTime(), "b@acme.com");
+//  	List<Employee> aa = new ArrayList<>();
+//  	aa.add(e);aa.add(b);
+//  	Assert.assertTrue(se.getEmployeeEmails(aa)[0].getAddress().equalsIgnoreCase("a@acme.com")); 
+//  }
+//    @Test
+//    public void sendMail() throws AddressException {
+//    	EmailMessageService se = new EmailMessageService();	
+//    	Employee e = new Employee("a", "", Calendar.getInstance().getTime(), "a@acme.com");
+//    	Employee b = new Employee("a", "", Calendar.getInstance().getTime(), "b@acme.com");
+//    	List<Employee> aa = new ArrayList<>();
+//    	aa.add(e);aa.add(b);
+//    	se.sendMessage(se.loadProps(), aa); 
+//    }
+		@Test
+		public void noCommandArgTest() {
+			MongoDatabaseService mongo = new MongoDatabaseService();
+			mongo.getMongoDB().getCollection("appraised").remove(new BasicDBObject() );
+			String[] strang = {""}; 
+			Notified.main(strang);
+			Assert.assertTrue("",mongo.getMongoDB().getCollection("appraised").getCount() > 0);
+			mongo.getMongoDB().getCollection("appraised").remove(new BasicDBObject() );
+			
+		}
 }
